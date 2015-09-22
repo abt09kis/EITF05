8f9980da37044074dae1c95d54cf3105920673ae
HampusBalldin <hampus.balldin@gmail.com>
2015-09-22 14:49:23 +0200

    Added hash stuff

--- eitf05/register.php
+++ eitf05/register.php
@@ -1,5 +1,8 @@
 <?php
-	function generateSalt() {
+
+	// Comment out error_reporting in prod. env.
+	error_reporting(E_ALL);
+	function generateSalt($length) {
 		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	    	$charactersLength = strlen($characters);
 		$randomString = '';
@@ -9,10 +12,20 @@
 		return $randomString;
 	}
 
+	function generateHash($pwd, $salt){
+		return hash_pbkdf2('sha256', $pwd, $salt, 1000, 0, false);
+	}
+
+
+	// Username must be unique
+	// Allowed characters password and username: [a-zA-Z0-9]
+	// Username at least 1 characters
+	// Password at least 10 characters
+		
 	$sql_user = ini_get("mysql.default_user");
 	$sql_host = ini_get("mysql.default_host");
 	$sql_pass = ini_get("mysql.default_password");
-
+	
 	$mysqli = new mysqli($sql_host, $sql_user, $sql_pass, "EITF05");
 	if ($mysqli->connect_errno) {
 		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -21,19 +34,18 @@
 	$sql = "INSERT INTO users(email, hash, salt, nbrAttempts) VALUES(?, ?, ?, '0')";
 	$stmt = $mysqli->prepare($sql);
 
-	$uid = $_POST['username'];
+	echo $sql;
+	$email = $_POST['username'];
 	$pwd = $_POST['password'];	
-	$salt = generateSalt();
+	$salt = generateSalt(10);
+
+	echo 'salt: ' . $salt;
 	$hash = generateHash($pwd, $salt);
 
-	if($stmt->bind_param('ss', $uid, $pwd)){
+	echo 'email: ' . $email . ', salt: ' . $salt . ', hash ' . $hash;
+	if($stmt->bind_param('sss', $email, $hash, $salt)){
+		echo "Bound params success <br/>";
 		if($stmt->execute()){
-			$res_uid = NULL;
-			$res_pwd = NULL;
-			$stmt->bind_result($res_uid, $res_pwd);
-			while ($stmt->fetch()) {
-			    echo 'uid = ' . $res_uid . ', pwd = ' . $res_pwd;
-			}
 			$stmt->free_result();
 		}
 	}
