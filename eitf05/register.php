c8e72872437cb7742ab3780414f4068c6f928d88
HampusBalldin <hampus.balldin@gmail.com>
2015-09-24 18:37:21 +0200

    Added regex ... refactored ...

--- eitf05/register.php
+++ eitf05/register.php
@@ -3,33 +3,52 @@
 	error_reporting(E_ALL);
 	include_once "database.php";
 	include_once "crypto.php";
+	
+	/*
+	* 10 to 64 characters in [1-zA-Z0-9]
+	* At least one lower case, one upper case and one digit.
+	*/
+	function isValidPassword($pwd){
+		$regex = "/\A(?=[a-zA-Z0-9]{10,64}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=[^0-9]*[0-9])/";
+		$combined = preg_match($regex, $pwd);
+		return $combined;
+	}
 
-	// Username must be unique
-	// Allowed characters password and username: [a-zA-Z0-9]
-	// Username at least 1 characters
-	// Password at least 10 characters
-	$db = new Database();
-	$mysqli = $db->openConnection();
-
-	$sql = "INSERT INTO users(email, hash, salt, nbrAttempts) VALUES(?, ?, ?, '0')";
-	$stmt = $mysqli->prepare($sql);
-
-	echo $sql;
+	/*
+	* 1 to 64 characters in [1-zA-Z0-9]
+	*/
+	function isValidUsername($uid){
+		$regex = "/\A(?=[a-zA-Z0-9]{1,64}\z)/";
+		$combined = preg_match($regex, $uid);
+		return $combined;
+	}
+	
 	$email = $_POST['username'];
-	$pwd = $_POST['password'];	
+	$pwd = $_POST['password'];
 
-	$crypto = new Crypto();
-	$salt = $crypto->generateSalt(10);
+	if(isValidPassword($pwd) && isValidUsername($email)) {
+		echo "Password ok";
+		$db = new Database();
+		$mysqli = $db->openConnection();
 
-	echo 'salt: ' . $salt;
-	$hash = $crypto->generateHash($pwd, $salt);
+		$sql = "INSERT INTO users(email, hash, salt, nbrAttempts) VALUES(?, ?, ?, '0')";
+		$stmt = $mysqli->prepare($sql);
 
-	echo 'email: ' . $email . ', salt: ' . $salt . ', hash ' . $hash;
-	if($stmt->bind_param('sss', $email, $hash, $salt)){
-		echo "Bound params success <br/>";
-		if($stmt->execute()){
-			$stmt->free_result();
+		$crypto = new Crypto();
+		$salt = $crypto->generateSalt(10);
+
+		echo 'salt: ' . $salt;
+		$hash = $crypto->generateHash($pwd, $salt);
+
+		echo 'email: ' . $email . ', salt: ' . $salt . ', hash ' . $hash;
+		if($stmt->bind_param('sss', $email, $hash, $salt)){
+			echo "Bound params success <br/>";
+			if($stmt->execute()){
+				$stmt->free_result();
+			}
 		}
+	}else {
+		echo "Password must be at least 10 characters long";
 	}
 	$mysqli->close();
 ?>
