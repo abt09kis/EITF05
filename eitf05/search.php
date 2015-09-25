be701d5aa4b1854714167a39d0423ffdab5682b2
DAnd91 <davidandersson91@me.com>
2015-09-25 12:04:42 +0200

    Fungerande searchsida.

--- eitf05/search.php
+++ eitf05/search.php
@@ -2,75 +2,51 @@
 
   <?php
 
-print "1";
-
   echo "<html>";
   echo "<body>";
   echo "<table style='width:100%' id = 'itemTable'>";
 
-  print "2";
-
-  $sql_user = ini_get("mysql.default_user");
-  $sql_host = ini_get("mysql.default_host");
-  $sql_pass = ini_get("mysql.default_password");
-
-  print "3";
+  $sql_user = "root";
+  $sql_host = "localhost";
+  $sql_pass = "root";
 
 
-    $mysqli = new mysqli($sql_host, $sql_user, $sql_pass, "EITF05");
+    $mysqli = new mysqli($sql_host, $sql_user, $sql_pass, "test");
     if ($mysqli->connect_errno) {
       echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
 
-    print "4";
-
     $sql = "SELECT * FROM items WHERE itemName = ?";
-
-    print "5";
 
     $stmt = $mysqli->prepare($sql);
 
-    print "6";
-
     $search = strip_tags($_POST['searchField']);
 
-    print  $search;
+    if($stmt->bind_param('s', $search)){
+      if($stmt->execute()){
+        $itemId = NULL;
+        $itemName = NULL;
+        $cost = NULL;
+        $stmt->bind_result($itemId, $itemName,$cost);
 
-    print "7";
+        print "<tr><th>Id</th><th>Name</th><th>cost</th></tr>";
 
-    $stmt->bind_param('s', $search);
+        if($stmt->fetch()) {
 
-    print "8";
+            print "<tr><th>".  $itemId ."</th><th>". $itemName."</th><th>". $cost."</th></tr>";
 
+        }else{
 
-    if(!$result = $mysqli->query($stmt)){
-
-        die('There was an error running the query [' . $db->error . ']');
-    }
-
-    print "9";
-
-
-    if ($result->num_rows > 0) {
-
-            echo "<tr><th>Id</th><th>Name</th><th>cost</th></tr>";
-
-            print "10";
-
-        while($row = $result->fetch_assoc()) {
-
-            echo "<tr><th>". $row["itemId"]."</th><th>". $row["itemName"]."</th><th>". $row["cost"]."</th></tr>";
-            $_SESSION["itemId"] = $row["itemId"];
+            print " 0 results for search: " . htmlspecialchars($search);
         }
 
-    } else {
+      }
 
-        print "0 results for search: " . htmlspecialchars($search);
     }
-    print "11";
-
+    $_SESSION["itemId"] = $itemId;
+    $stmt->free_result();
     $mysqli->close();
-    print "12";
+
 
     echo "</table>";
     echo "<form action='buy.php' method='POST'>";
@@ -78,7 +54,5 @@ print "1";
     echo "</form>";
     echo "</body>";
     echo "</html>";
-
-    print "13";
 
   ?>
