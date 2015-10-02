dcd1f9361bdee2636d76ad30821f5637f8364705
HampusBalldin <hampus.balldin@gmail.com>
2015-10-02 19:02:24 +0200

    Formatting

--- eitf05/search.php
+++ eitf05/search.php
@@ -1,60 +1,48 @@
 <?php
-include_once "database.php";
+	session_start();
+	include_once "database.php";
+	
+	echo "<html>";
+	echo "<body>";
+	echo "<table style='width:100%' id = 'itemTable'>";
 
-  session_start();
-  echo "<html>";
-  echo "<body>";
-  echo "<table style='width:100%' id = 'itemTable'>";
+	$database = new Database();
+	$database->openConnection();
+	
+	$sql = "SELECT * FROM items WHERE itemName = ?";
+	$stmt = $mysqli->prepare($sql);
+	$search = strip_tags($_POST['searchField']);
+	if($stmt->bind_param('s', $search)){
+		if($stmt->execute()){
+			$itemId = NULL;
+			$itemName = NULL;
+			$cost = NULL;
+			$stmt->bind_result($itemId, $itemName,$cost);
 
-  $database = new Database();
-  $database->openConnection();
+			print "<tr><th>Id</th><th>Name</th><th>cost</th></tr>";
+			if($stmt->fetch()) {
+				    print "<tr><th>".  $itemId ."</th><th>". $itemName."</th><th>". $cost."</th></tr>";
+			}else{
+			    print " 0 results for search: " . htmlspecialchars($search);
+			}
+		}
+	}
+	$_SESSION['itemId'] = $itemId;
+	$_SESSION['itemName'] = $itemName;
+	$stmt->free_result();
+	$database->closeConnection();
 
-    $sql = "SELECT * FROM items WHERE itemName = ?";
-
-    $stmt = $mysqli->prepare($sql);
-
-    $search = strip_tags($_POST['searchField']);
-
-    if($stmt->bind_param('s', $search)){
-      if($stmt->execute()){
-        $itemId = NULL;
-        $itemName = NULL;
-        $cost = NULL;
-        $stmt->bind_result($itemId, $itemName,$cost);
-
-        print "<tr><th>Id</th><th>Name</th><th>cost</th></tr>";
-
-        if($stmt->fetch()) {
-
-            print "<tr><th>".  $itemId ."</th><th>". $itemName."</th><th>". $cost."</th></tr>";
-
-        }else{
-
-            print " 0 results for search: " . htmlspecialchars($search);
-
-        }
-      }
-    }
-
-    $_SESSION['itemId'] = $itemId;
-    $_SESSION['itemName'] = $itemName;
-
-    $stmt->free_result();
-    $database->closeConnection();
-
-
-    echo "</table>";
-    echo "<form action='addToCart.php' method='POST'>";
-    echo "<input id='submit' type='submit' value='Add to cart' name= 'Add to cart'>";
-    echo "</form>";
-    echo "<form action='checkout.php' method='POST'>";
-    echo "<input id='submit' type='submit' value='Checkout' name= 'Checkout'>";
-    echo "</form>";
-    echo "</form>";
-    echo "<form action='search.html' method='POST'>";
-    echo "<input id='submit' type='submit' value='back' name= 'back'>";
-    echo "</form>";
-    echo "</body>";
-    echo "</html>";
-
-  ?>
+	echo "</table>";
+	echo "<form action='addToCart.php' method='POST'>";
+	echo "<input id='submit' type='submit' value='Add to cart' name= 'Add to cart'>";
+	echo "</form>";
+	echo "<form action='checkout.php' method='POST'>";
+	echo "<input id='submit' type='submit' value='Checkout' name= 'Checkout'>";
+	echo "</form>";
+	echo "</form>";
+	echo "<form action='search.html' method='POST'>";
+	echo "<input id='submit' type='submit' value='back' name= 'back'>";
+	echo "</form>";
+	echo "</body>";
+	echo "</html>";
+?>
