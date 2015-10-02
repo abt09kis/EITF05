b33dd09be5a2ce857b82abb9e672ee63e11f4935
HampusBalldin <hampus.balldin@gmail.com>
2015-10-02 11:26:40 +0200

    Bunch of things

--- eitf05/login.php
+++ eitf05/login.php
@@ -55,10 +55,36 @@
 
 		echo '<br/>Generated hash: ' . $hash . '<br/>';
 		echo 'Hash From db ' . $hash_db;
+
+		$ip = clientIP();
+
+		echo '<br/>IP = ' . $ip . ' attempt for ' . $username;
+
+
 		if($res_pwd == $hash){
 			header("Location: http://localhost:80/meet2eat/login.php");
 		}
 	}
+
+	function clientIP() {
+	    $ipaddress = '';
+	    if (getenv('HTTP_CLIENT_IP'))
+		$ipaddress = getenv('HTTP_CLIENT_IP');
+	    else if(getenv('HTTP_X_FORWARDED_FOR'))
+		$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+	    else if(getenv('HTTP_X_FORWARDED'))
+		$ipaddress = getenv('HTTP_X_FORWARDED');
+	    else if(getenv('HTTP_FORWARDED_FOR'))
+		$ipaddress = getenv('HTTP_FORWARDED_FOR');
+	    else if(getenv('HTTP_FORWARDED'))
+	       $ipaddress = getenv('HTTP_FORWARDED');
+	    else if(getenv('REMOTE_ADDR'))
+		$ipaddress = getenv('REMOTE_ADDR');
+	    else
+		$ipaddress = 'UNKNOWN';
+	    return $ipaddress;
+	}
+
 	Login();
 ?>
 
--- eitf05/search.php
+++ eitf05/search.php
@@ -8,12 +8,12 @@
 
   session_start();
     
-  $sql_user = "root";
-  $sql_host = "localhost";
-  $sql_pass = "root";
+  $sql_user = "eitf05usr";
+  $sql_host = "127.0.0.1";
+  $sql_pass = "eitf05pass";
 
 
-    $mysqli = new mysqli($sql_host, $sql_user, $sql_pass, "test");
+    $mysqli = new mysqli($sql_host, $sql_user, $sql_pass, "EITF05");
     if ($mysqli->connect_errno) {
       echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
