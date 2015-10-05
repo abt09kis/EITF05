e0c73abfee2ab2f2ffff68f586f469ce861469f7
dat11ge1 <dat11ge1@student.lu.se>
2015-10-05 15:28:39 +0200

    csrf
    Added csrf attack and fake index site

---
+++ eitf05Vulnerable/attackerDir/csrf.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+	// logout() will log the user out, clear all session variables and unset coookies in addition to terminating the session.
+		$_SESSION = array();
+		if(ini_get('session.use_cookies')){
+			$params = session_get_cookie_params();
+			setcookie(session_name(), ' ', time() - 42000, $params['path'], $params['domain'],$params['secure'], $params['httponly']);
+		}
+	session_destroy();
+	header("https://127.0.0.1:4433/");
+?>
